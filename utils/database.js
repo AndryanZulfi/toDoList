87203a15efc062c799810ef893b4d1d241ba51e6
AndryanZulfi <andryanzulfi2007@gmail.com>
2024-12-06 23:48:49 +0700

    edit database

--- utils/database.js
+++ utils/database.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://andryanzulfi:andryan123@cluster0.dhc8n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-
+mongoose.connect("mongodb+srv://andryanzulfi:andryan123@cluster0.dhc8n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Failed to connect to MongoDB', err));
